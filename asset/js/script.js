a9bfd4c4d4bca42b5292d7373108742e57e8d505
DeyvesDion <deyvesdion@gmail.com>
2022-04-22 20:03:47 +0200

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,3 +6,12 @@ const closeDesktop = document.querySelector(".closeBtnDesktop");
 // fonction pour definir le delais de overlay
 window.addEventListener("load", () => { setTimeout(() => { getOverlay.classList.add("show"); }, 1000) });
 closeDesktop.addEventListener("click", () => { getOverlay.classList.remove("show"); });
+
+window.addEventListener("scroll", () => {
+    // fonction menu sticky
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+    // Fonction toogle float-img
+    // const ShowImgToggle = document.querySelector(".float-img");
+    // ShowImgToggle.classList.toggle("show-img", window.scrollY >= 0)
+});
