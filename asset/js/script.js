48d258ceb4b929cdf0f169ed44ea149050a8ba3b
DeyvesDion <deyvesdion@gmail.com>
2022-10-09 18:38:51 +0200

    v2 du portfolio

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,15 @@
-// Declaration des variables
-const getOverlay = document.querySelector(".overlay");
-const closeDesktop = document.querySelector(".closeBtnDesktop");
-
-
-// fonction pour definir le delais de overlay
-window.addEventListener("load", () => { setTimeout(() => { getOverlay.classList.add("show"); }, 1000) });
-closeDesktop.addEventListener("click", () => { getOverlay.classList.remove("show"); });
-
+let typed = new Typed(".auto-type", {
+    strings: [
+        "Développeur Angular - Vue.js",
+        "Webmaster",
+        "Intégrateur Web"
+    ],
+    typeSpeed: 80,
+    backSpeed: 70,
+    loop: true,
+})
 window.addEventListener("scroll", () => {
     // fonction menu sticky
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-    // Fonction toogle float-img
-    // const ShowImgToggle = document.querySelector(".float-img");
-    // ShowImgToggle.classList.toggle("show-img", window.scrollY >= 0)
 });
